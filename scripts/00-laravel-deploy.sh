fe56e658aeedd5097153ca7c413a0646f72cac7e
leo johriel rivera <leojohrielrivera17@gmail.com>
2024-04-28 15:53:57 +0800

    change dockerfile

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 echo "Running composer"
+cp /etc/secrets/.env .env
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+echo "Clearing caches..."
+php artisan optimize:clear
 
 echo "Caching config..."
 php artisan config:cache
@@ -12,7 +13,7 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running npm install"
-npm install && npm run build
-# echo "Running migrations..."
-# php artisan migrate --force
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "done deploying"
