e8780186f0ebbfd2a7a74bc8039d1c458bf7bf69
leo johriel rivera <leojohrielrivera17@gmail.com>
2024-04-28 17:11:54 +0800

    added npm

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,6 +13,15 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "downloading frontend .."
+
+sudo apt update
+
+# Install Node.js
+sudo apt install -y nodejs
+
+# Install npm
+sudo apt install -y npm
+
 npm install && npm run build
 
 # echo "Running migrations..."
