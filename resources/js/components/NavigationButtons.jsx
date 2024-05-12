2150c1bad9f2ef0316bfa5a8c1b36cd0235a72e7
leo johriel rivera <leojohrielrivera17@gmail.com>
2024-05-12 18:04:00 +0800

    change the back and forward navigation

--- resources/js/components/NavigationButtons.jsx
+++ resources/js/components/NavigationButtons.jsx
@@ -12,12 +12,12 @@ const NavigationButtons = () => {
 
   return (
     <>
-    <div>
-        <button className='btn btn-info' onClick={goBack}><i className="fas fa-backward"></i></button>
+    <div className='col-md-3 col-6'>
+        <button className='btn btn-danger w-100' onClick={goBack}><i className="fas fa-backward"></i> BACK</button>
     </div>
-    <div className='clearfix mx-2'></div>
-    <div>
-        <button className='btn btn-warning' onClick={goForward}><i className="fas fa-forward"></i></button>
+    <div className='clearfix'></div>
+    <div className='col-md-3 col-6'>
+        <button className='btn btn-info w-100' onClick={goForward}>FORWARD <i className="fas fa-forward"></i></button>
     </div>
         
     </>
